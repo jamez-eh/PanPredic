1043c68e5b0bf2c9c8792a790765d04988f145c4
James Hopkins <jamesedhopkins@gmail.com>
2017-06-02 14:28:19 -0600

    "FIX: Issue with parsing large file in uploader.py"

--- modules/uploader.py
+++ modules/uploader.py
@@ -1,9 +1,12 @@
 
 from Bio import SeqIO
+from os.path import basename
 import os
+import sys
 import pandas as pd
 import json
 import re
+from definitions import ROOT_DIR
 
 
 
@@ -16,21 +19,26 @@ def parse_pan(file):
     :param :
         A tab delimited csv file from a panseq output
     :return: 
-        A pandas dataframe with LocusID and Allele dropped as well as empty rows
+        writes a file with reduced headers and the NA's dropped
     """
-
-    df = pd.read_csv(file, sep=None)
-
+    #the file size is too big to read in all at once
+    chunksize = 10 ** 6
+    for chunk in pd.read_csv(file, sep=None, chunksize=chunksize):
     #drop columns we don't need
-    df = df.drop(['LocusName', 'Allele'], axis=1)
-
+        chunk = chunk.drop(['LocusName', 'Allele'], axis=1)
     #drop rows we don't need
-    df = df.dropna(axis=0, how='any')
-    return df
+        chunk = chunk.dropna(axis=0, how='any')
+    # if file does not exist write header
+        if not os.path.isfile('filename.csv'):
+            chunk.to_csv(ROOT_DIR + '/tests/data/genparsed.txt', header='column_names',index=False)
+        else:  # else it exists so append without writing the header
+            chunk.to_csv(ROOT_DIR + '/tests/data/genparsed.txt', mode='a', header=False, index=False)
 
+
+parse_pan('/home/james/GenbankData/pan_genome.txt')
 
 #turn a datafarame to a list of rows lists
-def pan_to_dict(df):
+def pan_to_dict(file):
 
     """
     :param:
@@ -39,8 +47,7 @@ def pan_to_dict(df):
         A dictionary in the format for superphy to accept to add to blazegraph 
         ex. {'Some_Contig_ID':[{'START','STOP','ORIENTATION','GENE_NAME'}, {}, ....], etc}
     """
-
-    parsed_list = []
+    df = pd.read_csv(file, sep=None)
     i = 0
     prev_ID = None
     pan_dict = {}
@@ -62,8 +69,7 @@ def pan_to_dict(df):
         row_dict['GENOME'] = pan_list[1]
         row_dict['START'] = pan_list[2]
         row_dict['STOP'] = pan_list[3]
-        row_dict["PanID"] = i
-        contig_name = pan_list[4]
+        contig_name = contig_name_parse(pan_list[4])
 
         if contig_name in pan_dict:
             pan_dict[contig_name].append(row_dict)
@@ -102,7 +108,6 @@ def merge_dicts(sequence_dict, pan_dict):
 
 '''
 
-
 def contig_name_parse(pan_contig):
     """
     The panseq contig name is unhelpful
@@ -113,10 +118,12 @@ def contig_name_parse(pan_contig):
     """
 
     m = re.search('(?<=\|)(.*?)(?=_E)', pan_contig)
-    # print(m.group(0))
+    #print(m.group(0))
     m = re.search('(?<=\|).+', m.group(0))
 
     return m.group(0)
+
+
 
 def json_dump(file, dict):
     with open(file, 'w') as fp:
@@ -172,5 +179,10 @@ def main(dr):
     json_dump('hash_dict.json', d)
 
 
+'''
+gd = app.modules.turtleGrapher.turtle_grapher.generate_turtle_skeleton(sys.argv[1])
+data = gd.serialize(format="turtle")
+print(data)
+'''
 
-
+workflow('/home/james/GenbankData/pan_genome.txt')
