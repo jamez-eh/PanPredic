a980631d79ed84ba507b7671fdc1242d35638cd3
James Hopkin <jamesedhopkins@gmail.com>
2017-06-07 08:52:48 -0600

    "FIX: generation of pan config file, had wrong arguments"

--- modules/conf_gen.py
+++ modules/conf_gen.py
@@ -7,40 +7,40 @@ from definitions import ROOT_DIR, PAN_RESULTS, NOVEL_RESULTS
 
 def gen_novel(genome_files):
 
-    with open(ROOT_DIR + 'panseq_findnew_pangenome.conf2', 'wb') as f:
-        f.writelines([b'queryDirectory   ' + genome_files.encode(),
-                    b'referenceDirectory '	+ ROOT_DIR.encode() + b'/PanGenomeRef',
-                    b'baseDirectory  ' + NOVEL_RESULTS.encode(),
-                    b'numberOfCores	4',
-                    b'mummerDirectory	/home/james/pan_genome/PanPredic/MUMmer3.23/',
-                    b'blastDirectory	/home/james/pan_genome/PanPredic/ncbi-blast-2.6.0+/bin/',
-                    b'minimumNovelRegionSize	500',
-                    b'novelRegionFinderMode	no_duplicates',
-                    b'muscleExecutable	/home/james/pan_genome/PanPredic/muscle3.8.31_i86linux64',
-                    b'percentIdentityCutoff	90',
-                    b'runMode	novel',
+    with open(ROOT_DIR + '/panseq_novel_pangenome.conf', 'wb') as f:
+        f.writelines([b'queryDirectory   ' + genome_files.encode() + b'\n',
+                    b'referenceDirectory '	+ ROOT_DIR.encode() + b'/PanGenomeRef \n',
+                    b'baseDirectory  ' ROOT_DIR.encode() + NOVEL_RESULTS.encode() + b'\n',
+                    b'numberOfCores	4 \n',
+                    b'mummerDirectory	/home/james/pan_genome/PanPredic/MUMmer3.23/ \n',
+                    b'blastDirectory	/home/james/pan_genome/PanPredic/ncbi-blast-2.6.0+/bin/ \n',
+                    b'minimumNovelRegionSize	500 \n',
+                    b'novelRegionFinderMode	no_duplicates \n',
+                    b'muscleExecutable	/home/james/pan_genome/PanPredic/muscle3.8.31_i86linux64 \n',
+                    b'percentIdentityCutoff	90 \n',
+                    b'runMode	novel \n',
                     b'overwrite 1'])
-        return f
+        return ROOT_DIR + '/panseq_novel_pangenome.conf'
 
 
 def gen_match(genome_files):
-    with open(ROOT_DIR + 'panseq_match_pangenome2.conf', 'wb') as f:
-        f.writelines([b'queryDirectory   ' + genome_files.encode(),
+    with open(ROOT_DIR + '/panseq_pan_pangenome.conf', 'wb') as f:
+        f.writelines([b'queryDirectory   ' + genome_files.encode() + b'\n',
                     #'queryFile  ' + ROOT_DIR + '/PanGenomeRef/coreGenomeFragments.fasta',
-                    b'baseDirectory  ' +  PAN_RESULTS.encode(),
-                    b'numberOfCores	4',
-                    b'mummerDirectory	/home/james/pan_genome/PanPredic/MUMmer3.23/',
-                    b'blastDirectory	/home/james/pan_genome/PanPredic/ncbi-blast-2.6.0+/bin/',
-                    b'minimumNovelRegionSize	500',
-                    b'muscleExecutable	/home/james/pan_genome/PanPredic/muscle3.8.31_i86linux64',
-                    b'novelRegionFinderMode	no_duplicates',
-                    b'muscleExecutable	/home/james/pan_genome/PanPredic/muscle3.8.31_i86linux64',
-                    b'fragmentationSize	500',
-                    b'percentIdentityCutoff	90',
-                    b'runMode	pan',
-                    b'nameOrId	name',
-                    b'overwrite 1'])
-        return f
+                    b'baseDirectory  ' +  PAN_RESULTS.encode() + b'\n',
+                    b'numberOfCores	4 \n',
+                    b'mummerDirectory	/home/james/pan_genome/PanPredic/MUMmer3.23/ \n',
+                    b'blastDirectory	/home/james/pan_genome/PanPredic/ncbi-blast-2.6.0+/bin/ \n',
+                    b'minimumNovelRegionSize	500 \n',
+                    b'muscleExecutable	/home/james/pan_genome/PanPredic/muscle3.8.31_i86linux64 \n',
+                    b'novelRegionFinderMode	no_duplicates \n',
+                    b'fragmentationSize	500 \n',
+                    b'percentIdentityCutoff	90 \n',
+                    b'coreGenomeThreshold   10000000',
+                    b'runMode	pan \n',
+                    b'nameOrId	id \n',
+                    b'overwrite 1 \n'])
+        return ROOT_DIR + '/panseq_pan_pangenome.conf'
 
 #driver function generates both necessary conf files
 def generate_conf(genome_files):
