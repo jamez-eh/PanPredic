20bfd368e8fd52ab4d22f779ceb10b88ff57a62c
James Hopkin <jamesedhopkins@gmail.com>
2017-06-07 14:34:08 -0600

    "UPDATE: changed turtlegrapher to give pangenome regions a sequence"

--- modules/main_queue.py
+++ modules/main_queue.py
@@ -1,24 +1,27 @@
-import modules.pan_run
-import modules.uploader
-import modules.conf_gen
-from definitions import PAN_RESULTS, NOVEL_RESULTS
+from app.modules.PanPredic.modules.pan_run import panseq
+from app.modules.PanPredic.modules.uploader import workflow
+from app.modules.PanPredic.modules.conf_gen import generate_conf
+from app.modules.PanPredic.definitions import PAN_RESULTS, NOVEL_RESULTS
 
 def pan(args_dict):
 
     # (1) generate conf files
-    query_dict = modules.conf_gen.generate_conf(args_dict['genomes'])
+    query_dict = generate_conf(args_dict['i'])
 
     # (2) run panseq
-    modules.pan_run.panseq(query_dict)
+    panseq(query_dict)
 
     # (3) Parse panseq results
-    results_pickle = modules.uploader.workflow(PAN_RESULTS + '/pan_genome.txt', PAN_RESULTS + '/coreGenomeFragments.fasta')
+    results_pickle = workflow(PAN_RESULTS + '/pan_genome.txt', PAN_RESULTS + '/coreGenomeFragments.fasta')
+
+    return results_pickle
 
     # (4) upload pan data to blazegraph
 
     # (5) analyze pan data
-
+'''
 dict = {}
 dict['genomes'] = '/home/james/backend/app/modules/PanPredic/tests/data/filteredgenomes'
 
 pan(dict)
+'''
