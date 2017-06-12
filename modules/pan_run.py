07fecc3088852428a0358f6a2afddb4fd2c5c9d5
James Hopkin <jamesedhopkins@gmail.com>
2017-06-12 15:06:28 -0600

    "ADD: function to pull set of pangenome regions and sequences"

--- modules/pan_run.py
+++ modules/pan_run.py
@@ -47,20 +47,3 @@ def join_files(file1, file2):
 
 
 
-#queries database and builds a pangenome
-def build_pan():
-    g = rdflib.Graph()
-
-    # ... add some triples to g somehow ...
-    g.parse("some_foaf_file.rdf")
-
-    qres = g.query(
-        """SELECT DISTINCT ?aname ?bname
-           WHERE {
-              ?a foaf:knows ?b .
-              ?a foaf:name ?aname .
-              ?b foaf:name ?bname .
-           }""")
-
-    for row in qres:
-        print("%s knows %s" % row)
--- modules/queries.py
+++ modules/queries.py
@@ -1,9 +1,33 @@
 import config
 from os import path
 import sys
+import rdflib
 sys.path.append(path.abspath('/home/james/backend/app/'))
+from app.modules.groupComparisons.decorators import submit, tolist, prefix
+from app.modules.turtleGrapher.turtle_utils import link_uris as gu
 
-from app.module import groupComparisons
 
 
 blazegraph_url = config.database['blazegraph_url']
+
+#queries database and builds a pangenome
+@tolist
+@submit
+@prefix
+def query_panseq():
+    '''
+    Grabs all objectids having the relation.
+    '''
+    query = """
+    SELECT ?p ?s WHERE {{
+          	?p a :PanGenomeRegion .
+   			?p g:DNASequence ?s .
+       
+    }}
+    """
+    return query
+
+
+
+list = query_panseq()
+
