e1b2af524b3deb990d533222139202f5eb72b76f
James Hopkin <jamesedhopkins@gmail.com>
2017-06-13 14:34:06 -0600

    "ADD: all queries necessary to get datasets for ML modules"

--- modules/queries.py
+++ modules/queries.py
@@ -3,6 +3,7 @@ from os import path
 import sys
 sys.path.append(path.abspath('/home/james/backend/app/'))
 from app.modules.groupComparisons.decorators import submit, tolist, prefix
+from app.modules.turtleGrapher.turtle_utils import generate_uri as gu
 
 
 
@@ -21,9 +22,57 @@ def query_panseq():
        
     }}
     """
+
+# gets pangenomeregions for a single genome
+@tolist
+@submit
+@prefix
+def get_pangenome(genome):
+    '''
+    Grabs all objectids having the relation.
+    '''
+    query = """
+    SELECT ?p WHERE {{
+          	?p a :PanGenomeRegion .
+   			?p :isFoundIn <{genome}> .
+
+    }}
+    """.format(genome = gu(genome))
+    print(query)
     return query
 
 
-#query the entire pangenome and get
+@tolist
+@submit
+@prefix
+def get_genomes(pan):
+    '''
+    Grabs all objectids having the relation.
+    '''
+    query = """
+    SELECT ?p WHERE {{
+          	?p a g:Genome .
+   			?p :hasPart <{PanGenomeRegion}> .
+
+    }}
+    """.format(PanGenomeRegion = pan)
+    print(query)
+    return query
 
 
+@tolist
+@submit
+@prefix
+def get_virulence(genome):
+    '''
+    Grabs all objectids having the relation.
+    '''
+    query = """
+    SELECT ?p WHERE {{
+          	?p a :VirulenceFactor .
+   			?p :isFoundIn <{genome}> .
+
+    }}
+    """.format(genome = genome)
+    print(query)
+    return query
