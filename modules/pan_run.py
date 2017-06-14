bb4e40e6416c377d6cef8b620f9ce7ecaa1c4fa8
James Hopkin <jamesedhopkins@gmail.com>
2017-06-14 16:04:34 -0600

    "FIX: paths for joining the novel regions to the pangenome from blazegraph "

--- modules/pan_run.py
+++ modules/pan_run.py
@@ -3,6 +3,7 @@ import sys
 import shutil
 from app.modules.PanPredic.definitions import ROOT_DIR
 from app.modules.PanPredic.modules.queries import query_panseq
+import os
 
 
 args = 'perl lib/panseq.pl settings.txt'
@@ -16,20 +17,24 @@ def panseq(query_dict):
     pan_list = query_panseq()
 
     if pan_list:
+        #build the fasta file required as a queryfile for the pan run
+        query_file = build_pan(pan_list)
 
         #run panseq to find novel pangenome regions
         novel = subprocess.Popen(["perl", "/home/james/Panseq/lib/panseq.pl", novel_config], stdout=sys.stdout)
         novel.communicate()
 
-        #build the fasta file required as a queryfile for the pan run
-        query_file = build_pan(pan_list)
+
 
         # append these pangenome regions to current pangenome fastareference
-        join_files(query_file, novel_config)
+        join_files(query_file, ROOT_DIR + '/tests/data/novelResults/novelRegions.fasta')
 
     #finds a full set of pangenome regions for the queried genomes
     match = subprocess.Popen(["perl", "/home/james/Panseq/lib/panseq.pl", match_config], stdout=sys.stdout)
+
     match.communicate()
+
+    os.remove(query_file)
 
 
 
@@ -67,10 +72,10 @@ def build_pan(pan_list):
     i = 1
     for entry in pan_list:
         if i % 2 != 0:
-            f = open(filename, "a")
+            f = open(filename, "a+")
             f.write('>' + entry + '\n')
         else:
-            f = open(filename, "a")
+            f = open(filename, "a+")
             f.write(entry + '\n')
         f.close()
         i = i + 1
